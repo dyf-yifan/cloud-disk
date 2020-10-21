16a761f513d853bd9331ac84ce1fbdfc73331228
dingyifan <510040515@qq.com>
2020-10-21 22:58:41 +0800

    添加资源文件，封装自定义导航栏，使用slot传递自定义导航中左右的内容，并固定其位置，修改index

--- cloud-disk/pages/index/index.vue
+++ cloud-disk/pages/index/index.vue
@@ -1,30 +1,27 @@
 <template>
 	<view>
-		<!-- 自定义导航栏 -->
-		<view>
-			<uni-status-bar></uni-status-bar>
-		<view class="flex border-bottom align-center" style="height: 44px;">
-			<view class="flex-1 flex">
-				<text class="font-md ml-3">首页</text>
-			</view>
-			<view class="flex-1 flex justify-center">
-				
-			</view>
-			<view class="flex-1 flex justify-end">
-				<view class="flex align-center justify-center bg-light rounded-circle mr-3" style="width: 60rpx;height: 60rpx;">
+	<!-- 自定义导航栏 -->
+		<nav-bar>
+			<text slot="left" class="font-md ml-3">首页</text>
+			<template slot="right">
+				<view style="width: 60rpx;height: 60rpx;"
+					  class="flex align-center justify-center bg-icon rounded-circle mr-3">
 					<text class="iconfont icon-zengjia"></text>
 				</view>
-				<view class="flex align-center justify-center bg-light rounded-circle mr-3" style="width: 60rpx;height: 60rpx;">
+				<view style="width: 60rpx;height: 60rpx;"
+					  class="flex align-center justify-center bg-icon rounded-circle mr-3">
 					<text class="iconfont icon-gengduo"></text>
 				</view>
-			</view>
-		</view>
+			</template>
+		</nav-bar>
+		<view class="bg-info" style="height: 1000px;">
+			12322
 		</view>
 	</view>
 </template>
 
 <script>
-	import uniStatusBar from '@/components/uni-ui/uni-status-bar/uni-status-bar.vue'
+	import navBar from '@/components/common/nav-bar.vue';
 	export default {
 		data() {
 			return {
@@ -38,7 +35,7 @@
 
 		},
 		components:{
-			uniStatusBar
+			navBar
 		}
 	}
 </script>

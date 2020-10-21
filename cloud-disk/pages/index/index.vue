cd5f903e82ac5a8ef0987300cad44f6e6f1529aa
dingyifan <510040515@qq.com>
2020-10-21 22:43:25 +0800

    在页面中添加自定义导航内容

--- cloud-disk/pages/index/index.vue
+++ cloud-disk/pages/index/index.vue
@@ -1,13 +1,30 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view>
+		<!-- 自定义导航栏 -->
+		<view>
+			<uni-status-bar></uni-status-bar>
+		<view class="flex border-bottom align-center" style="height: 44px;">
+			<view class="flex-1 flex">
+				<text class="font-md ml-3">首页</text>
+			</view>
+			<view class="flex-1 flex justify-center">
+				
+			</view>
+			<view class="flex-1 flex justify-end">
+				<view class="flex align-center justify-center bg-light rounded-circle mr-3" style="width: 60rpx;height: 60rpx;">
+					<text class="iconfont icon-zengjia"></text>
+				</view>
+				<view class="flex align-center justify-center bg-light rounded-circle mr-3" style="width: 60rpx;height: 60rpx;">
+					<text class="iconfont icon-gengduo"></text>
+				</view>
+			</view>
+		</view>
 		</view>
 	</view>
 </template>
 
 <script>
+	import uniStatusBar from '@/components/uni-ui/uni-status-bar/uni-status-bar.vue'
 	export default {
 		data() {
 			return {
@@ -19,34 +36,13 @@
 		},
 		methods: {
 
+		},
+		components:{
+			uniStatusBar
 		}
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
 </style>

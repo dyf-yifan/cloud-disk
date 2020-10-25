a70a5a11d03043815bb9fb286bd2a3063d46d06e
dingyifan <510040515@qq.com>
2020-10-25 15:50:54 +0800

    重命名

--- egg-example/app/router.js
+++ egg-example/app/router.js
@@ -20,4 +20,5 @@ module.exports = app => {
 	router.get('/getsize', controller.user.getSize);
 	router.get('/file', controller.file.list);
 	router.post('/file/createdir', controller.file.createdir);
+	router.post('/file/rename', controller.file.rename);
 };

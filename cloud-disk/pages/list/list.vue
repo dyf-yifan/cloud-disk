cb5d4319f7a2db086bb107daf6b0460021929a27
dingyifan <510040515@qq.com>
2020-10-26 19:23:29 +0800

    上传进度的实现

--- cloud-disk/pages/list/list.vue
+++ cloud-disk/pages/list/list.vue
@@ -12,25 +12,46 @@
 		<swiper class="flex-1 flex" :duration="250" :current="tabIndex" @change="changeTab($event.detail.current)">
 			<swiper-item class="flex-1 flex" v-for="(item,index) in tabBars" :key="index">
 				<scroll-view scroll-y="true" class="flex-1">
-					<view class="bg-light flex align-center font-sm px-2 text-muted" style="height: 60rpx;">
+					<!-- 下载列表 -->
+					<template v-if="index === 0">
+						<view></view>
+					</template>
+					<!-- 上传列表 -->
+					<template v-else>
+						<view class="p-2 border-bottom border-light-secondary font text-muted">
+							上传中({{uploading.length}})
+						</view>
+						<f-list v-for="(item,index) in uploading" :key="'i'+index" :item="item" :index="index">
+							<view class="flex align-center text-main" style="height: 70rpx;">
+								<text class="iconfont icon-zanting"></text>
+								<text class="ml-1">{{item.progress}}%</text>
+							</view>
+							 <progress :percent="item.progress" slot="bottom" activeColor="#007BFF" :stroke-width="4" />
+						</f-list>
+						<view class="p-2 border-bottom border-light-secondary font text-muted">
+							上传完成{{uploaded.length}}
+						</view>
+						<f-list v-for="(item ,index) in uploaded" :key="'d'+index" :item="item" :index="index" :showRight="false"></f-list>
+					</template>
+					<!-- <view class="bg-light flex align-center font-sm px-2 text-muted" style="height: 60rpx;">
 						文件下载至：storage/XXXX/XXXX
 					</view>
 					<view class="bg-light flex align-center font-sm px-2 text-muted">
 						下载中({{downing.length}})
-					</view>
+					</view> -->
 					<!-- 注意 -->
-					<f-list v-for="(item,index) in downing" :key="'i'+index" :item="item" :index="index">
+				<!-- 	<f-list v-for="(item,index) in downing" :key="'i'+index" :item="item" :index="index">
 						<view class="flex align-center text-main" style="height: 70rpx;">
 							<text class="iconfont icon-zanting"></text>
 							<text class="ml-1">{{item.download}}%</text>
-						</view>
+						</view> -->
 						<!-- 进度条组件，uni-app自带的，无需引入。percent双属性绑定下载百分比数值 -->
-						<progress slot="bottom" :percent="item.download" activeColor="#009CFF" :stroke-width="4"></progress>
+					<!-- 	<progress slot="bottom" :percent="item.download" activeColor="#009CFF" :stroke-width="4"></progress>
 					</f-list>
 					<view class="p-2 border-bottom border-light-secondary font text-muted">
 						下载完成{{item.length}}
 					</view>
-					<f-list v-for="(item ,index) in downed" :key="'d'+index" :item="item" :index="index" :showRight="false"></f-list>
+					<f-list v-for="(item ,index) in downed" :key="'d'+index" :item="item" :index="index" :showRight="false"></f-list> -->
 				</scroll-view>
 			</swiper-item>
 		</swiper>
@@ -38,7 +59,8 @@
 </template>
 
 <script>
-import fList from '@/components/common/f-list.vue'
+import fList from '@/components/common/f-list.vue';
+import {mapState} from 'vuex';
 	export default {
 		data() {
 			return {
@@ -99,16 +121,29 @@ import fList from '@/components/common/f-list.vue'
 			fList
 		},
 		computed:{
-			downing() {
-				return this.list.filter(item => {
-					return item.download < 100;
-				})
+			...mapState({
+				uploadList: state => state.uploadList
+			}),
+			uploading(){
+				return this.uploadList.filter(item => {
+					return item.progress < 100;
+				});
 			},
-			downed() {
-				return this.list.filter(item => {
-					return item.download === 100;
-				})
+			uploaded(){
+				return this.uploadList.filter(item => {
+					return item.progress === 100;
+				});
 			}
+			// downing() {
+			// 	return this.list.filter(item => {
+			// 		return item.download < 100;
+			// 	})
+			// },
+			// downed() {
+			// 	return this.list.filter(item => {
+			// 		return item.download === 100;
+			// 	})
+			// }
 		}
 	}
 </script>
